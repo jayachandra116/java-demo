7ab063e3cdc914c7bf8f3031c4d36f26f1fba026
Jaya Chandra M <mjchandra97@gmail.com>
2026-04-05 12:48:39 +0000

    update greeting

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD");
+        System.out.println("Hello from Java Application!");
     }
 }
